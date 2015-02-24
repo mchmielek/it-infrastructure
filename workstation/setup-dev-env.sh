b6fee9cd1a1c779d7b554519f6b60669d9eb070c
Michal Chmielewski <mchmielek@gmail.com>
2015-02-24 22:33:30 +0100

    Setting up development workstation on Linux (initial version)

---
+++ workstation/setup-dev-env.sh
@@ -0,0 +1,8 @@
+install_virtualbox
+install_git
+
+sudo apt-get install git-gui
+sudo apt-get install aspell aspell-pl
+git config --global push.default simple
+git config --global user.name "Michal Chmielewski"
+git config --global user.email mchmielek@gmail.com
